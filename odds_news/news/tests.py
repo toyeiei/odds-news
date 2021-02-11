2468f5b5d673f3ebc00559adf5b63e1f4b373e8e
toyeiei <toy@datarockie.com>
2021-02-11 15:59:30 +0700

    Write test for NewsView

--- odds_news/news/tests.py
+++ odds_news/news/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import News 
+from categories.models import Category
 
 # Create your tests here.
+class TestNewsView(TestCase):
+    def test_view_should_see_title_and_category_name_and_content(self):
+        # Given
+        category = Category.objects.create(name='Data Science')
+        News.objects.create(title='ODDS', content='Hello', category=category) # create object in django
+        
+        # When
+        response = self.client.get('/news1/')
+        
+        # Then
+        assert 'ODDS' in str(response.content)
+        assert 'Hello' in str(response.content)
+        assert 'Data Science' in str(response.content)
