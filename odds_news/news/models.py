84bebc554c63fc34c6cf7131a5b0b47bad9ddc59
toyeiei <toy@datarockie.com>
2021-02-11 14:13:57 +0700

    Link news to category

--- odds_news/news/models.py
+++ odds_news/news/models.py
@@ -1,10 +1,17 @@
+# from odds_news.categories.models import Category
+
 from django.db import models
+from categories.models import Category # impact db needs to migrate again
 
 # Create your models here.
 # ORM
 class News(models.Model):
     title = models.CharField(max_length=300)
     content = models.TextField()
+
+    # key is the same name as model
+    # one to many relationship
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
