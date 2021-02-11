1b5596a9f4da43b32b27beadf22c1b60dd11e566
toyeiei <toy@datarockie.com>
2021-02-11 11:11:14 +0700

    Create news model

--- odds_news/news/models.py
+++ odds_news/news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# ORM
+class News(models.Model):
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
