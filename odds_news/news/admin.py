ab66e761bef969b04f48ed380a2393bd95064edc
toyeiei <toy@datarockie.com>
2021-02-11 13:34:52 +0700

    Register News to admin

--- odds_news/news/admin.py
+++ odds_news/news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import News
 
 # Register your models here.
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    # custom ModelAdmin
+    list_display = (
+        'title',
+        'content', # add comma at the end
+    )
